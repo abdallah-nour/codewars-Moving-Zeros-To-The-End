8530e8f9e3b36c97b3a2cd76c2d0fd6949dc8058
abdallah-nour <abdallah.nouraldin@gmail.com>
2022-08-16 06:38:46 +0300

    🚀 build moveZeros function

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function moveZeros(arr) {
-  return;
+function moveZeros(arr = []) {
+  const zeros = [];
+  const wihoutZeros = [];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (element === 0) zeros.push(element);
+    else wihoutZeros.push(element);
+  }
+  return [...wihoutZeros, ...zeros];
 }
 
 module.exports = moveZeros;
