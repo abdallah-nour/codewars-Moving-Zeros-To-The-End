d461f414f766b2b494d825c84fb48bfe9e375ae3
abdallah-nour <abdallah.nouraldin@gmail.com>
2022-08-16 06:47:00 +0300

    🐱‍🏍 add additional test cases

--- index.test.js
+++ index.test.js
@@ -4,10 +4,106 @@ const moveZeros = require('./index');
 config.truncateThreshold = 0;
 
 describe('Tests', () => {
-  it('test', () => {
+  it('fixed tests', () => {
     assert.deepEqual(
       moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]),
       [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
     );
+    assert.deepEqual(
+      moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]),
+      [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
+    );
+    assert.deepEqual(
+      moveZeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]),
+      [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    );
+    assert.deepEqual(
+      moveZeros([
+        'a',
+        0,
+        0,
+        'b',
+        'c',
+        'd',
+        0,
+        1,
+        0,
+        1,
+        0,
+        3,
+        0,
+        1,
+        9,
+        0,
+        0,
+        0,
+        0,
+        9,
+      ]),
+      ['a', 'b', 'c', 'd', 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    );
+    assert.deepEqual(
+      moveZeros([
+        'a',
+        0,
+        0,
+        'b',
+        null,
+        'c',
+        'd',
+        0,
+        1,
+        false,
+        0,
+        1,
+        0,
+        3,
+        [],
+        0,
+        1,
+        9,
+        0,
+        0,
+        {},
+        0,
+        0,
+        9,
+      ]),
+      [
+        'a',
+        'b',
+        null,
+        'c',
+        'd',
+        1,
+        false,
+        1,
+        3,
+        [],
+        1,
+        9,
+        {},
+        9,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ]
+    );
+    assert.deepEqual(moveZeros([0, 1, null, 2, false, 1, 0]), [
+      1,
+      null,
+      2,
+      false,
+      1,
+      0,
+      0,
+    ]);
   });
 });
