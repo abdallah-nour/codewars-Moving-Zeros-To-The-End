f5192054e2fa45081530c960cd7b8d3aa8897948
abdallah-nour <abdallah.nouraldin@gmail.com>
2022-08-16 08:39:45 +0300

    🚀 add short code moveZeros function

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function moveZeros(arr = []) {
+  return [...arr.filter((v) => v !== 0), ...arr.filter((v) => v === 0)];
+}
+
+function moveZeros2(arr = []) {
   const zeros = [];
   const wihoutZeros = [];
   for (let i = 0; i < arr.length; i++) {
